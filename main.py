6a66421d732703b7b7bc0fa48d845b4d23d166b2
Sunay P <sunayp2048@gmail.com>
2024-08-23 18:17:43 -0400

    had fun for a while drawing rectangles, then made screen work.

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
-import pygame
 from constants import *
 
 def main():
+    import pygame
+    import time
+    pygame.init
+    
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.fill("black")
 
-if __name__ == "__main__":
-    main()
+
+   
+    while True:
+        time.sleep(0.01)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        
+
+
+        pygame.display.flip()
+
+
+main()
